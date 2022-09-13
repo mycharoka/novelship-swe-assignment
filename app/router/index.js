5507a52dc190d2f30aca992e15b8648a3d6e63b8
muhammad.vidi <muhammad.vidi@delameta.co.id>
2022-09-13 14:27:25 +0700

    add redis middleware

--- app/router/index.js
+++ app/router/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const fibonacci = require('../fibonacci')
+const redisCheckCache = require('../redis/middleware')
 
-router.get('/:index', fibonacci.getFibonacciIdx)
+router.get('/:index', redisCheckCache.checkCache, fibonacci.getFibonacciIdx)
 
 module.exports = router
