f51a331999aef0859c5bd184bc4ae815fd7196a8
muhammad.vidi <muhammad.vidi@delameta.co.id>
2022-09-14 10:18:59 +0700

    update fibonacci function for better counting

--- app/fibonacci/index.js
+++ app/fibonacci/index.js
@@ -1,42 +1,60 @@
-const repo = require('./repository')
-const redis = require('redis')
+const repo = require("./repository");
+const redis = require("redis");
 
 async function getFibonacciIdx(req, res) {
-  const {index} = req.params
-  let client = redis.createClient(process.env.REDIS_PORT)
+  const { index } = req.params;
+  let client = redis.createClient(process.env.REDIS_PORT);
 
-  client.on('error', error => {
-    console.error(`Redis error : ${error}`)
-  })
+  client.on("error", (error) => {
+    console.error(`Redis error : ${error}`);
+  });
 
   let config = {
     baseUrl: `${req.baseUrl.toString()}`,
     originalUrl: `${req.originalUrl.toString()}`,
-    params_idx: `${JSON.stringify(req.params.index)}`
-  }
-  try { 
-    let result = fibonacci(index)
+    params_idx: `${JSON.stringify(req.params.index)}`,
+  };
+  try {
+    let result = fibonacci(index);
 
-    client.setex("fibIndex", 3600, result)
-    
+    client.setex("fibIndex", 3600, result);
+
     return res.json({
-      status: 'success',
-      result
-    })
+      status: "success",
+      result,
+    });
   } catch (error) {
     return res.json({
-      message: error.message
-    })
+      message: error.message,
+    });
   }
 }
 
 function fibonacci(index) {
-  if (index < 2) {
-    return index
+  // if (index < 2) {
+  //   return index
+  // }
+  // return fibonacci(index - 1) + fibonacci(index - 2)
+
+  let firstVal = 0;
+  let secondVal = 1;
+
+  let sum = 0;
+
+  for (let i = 2; i <= index; i++) {
+    sum = firstVal + secondVal;
+
+    firstVal = secondVal;
+
+    secondVal = sum;
   }
-  return fibonacci(index - 1) + fibonacci(index - 2)
+
+  const result = BigInt(index ? secondVal : firstVal).toString()
+
+  return result;
+
 }
 
 module.exports = {
-  getFibonacciIdx
-}
+  getFibonacciIdx,
+};
